331bf6a185df7882f04992a8323d58f6db1ce963
AngelKusik <angelica.kusik@dcmail.ca>
2023-01-19 15:47:05 -0500

    Added remaining files

---
+++ scripts/event-handler.js
@@ -0,0 +1,41 @@
+/* 
+* Week 2 - In class demo
+* January 19 
+**/
+
+var welcomeLabel = document.getElementById("welcome");
+var displayEvent = document.getElementById("displayEvent");
+
+var userDisplay = document.getElementById('userDisplay');
+var passDisplay = document.getElementById('passDisplay')
+
+
+welcomeLabel.addEventListener("dblclick", function () {
+    console.log("Event: double click");
+    displayEvent.textContent = "Event: double click";
+});
+
+welcomeLabel.addEventListener("mouseover", function () {
+    console.log("Event: click");
+    displayEvent.textContent = "Event: mouseover";
+});
+
+welcomeLabel.addEventListener("mouseout", function () {
+    console.log("Event: click");
+    displayEvent.textContent = "Event: mouseout";
+});
+
+function validateForm() {
+    let x = document.forms['myForm']['username'].value;
+    let y = document.forms['myForm']['password'].value;
+
+    if (x == ""){
+        alert("Name must be filled out");
+        return false;
+    } else {
+        console.log(`Username: ${ x } \n Password: ${ y }`)
+        userDisplay.textContext = x;
+        passDisplay.textContent = y;
+        return false;
+    }
+}
